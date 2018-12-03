f46c6cd8a952d1602699846dcf4b5d05ac707747
Gabriel Lebec <glebec@gmail.com>
2018-12-03 13:39:14 -0500

    feedback: backend assignment 1

--- route/index.js
+++ route/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const router = express.Router();
+const indexRouter = express.Router();
+const puppyRouter = express.Router();
 
-const Puppy = require('../db/model/puppies');
+const { Puppy } = require('../db/model/puppies');
 
-router.get('/', async (req, res, next) => {
+puppyRouter.get('/', async (req, res, next) => {
   try {
     const puppies = await Puppy.findAll();
     res.send(puppies);
@@ -12,7 +13,7 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-router.get('/:id', async (req, res, next) => {
+puppyRouter.get('/:id', async (req, res, next) => {
   try {
     let puppy = await Puppy.findById(req.params.id);
     if (puppy) {
@@ -25,7 +26,7 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
-router.delete('/:id', async (req, res, next) => {
+puppyRouter.delete('/:id', async (req, res, next) => {
     try {
       await Puppy.destroy({ where: { id: req.params.id } });
       res.status(204).send();
@@ -33,9 +34,9 @@ router.delete('/:id', async (req, res, next) => {
       next(err);
     }
   });
-  
 
-router.post('/', async (req, res, next) => {
+
+puppyRouter.post('/', async (req, res, next) => {
   try {
     let puppy = await Puppy.create(req.body);
     res.status(201).send(puppy);
@@ -44,7 +45,7 @@ router.post('/', async (req, res, next) => {
   }
 });
 
-router.put('/:id', async (req, res, next) => {
+puppyRouter.put('/:id', async (req, res, next) => {
   try {
     let updatedPuppyInfo = await Puppy.update(req.body, {
       where: { id: req.params.id },
@@ -57,4 +58,6 @@ router.put('/:id', async (req, res, next) => {
   }
 });
 
-module.exports = router;
+indexRouter.use('/puppies', puppyRouter)
+
+module.exports = indexRouter;
